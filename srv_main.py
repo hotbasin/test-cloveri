48561fd05cf0086c737213a0ea10beaaade7cfea
Stalk Spectrum <stalkspectrum@wildfielded.site>
2023-09-04 18:47:08 +0800

    Workday save4 04-09-2023

---
+++ srv_main.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+from bottle import HTTPError, get, post, request, run
+
+import srv_api as api_
+
+
+''' =====----- Server resources -----===== '''
+
+@post('/auth/login')
+def login_post() -> dict:
+    ''' Ресурс аутентификации на сервере через метод POST
+    '''
+    return api_.login_getpost(request.json)
+
+
+@get('/users/all')
+def all_abon_get() -> dict:
+    ''' Ресурс выдачи всей базы врачей и клиентов
+    '''
+    return api_.all_abon_get(req_data=request.query.req_data)
+
+
+@post('/coords/update')
+def coords_update_post() -> dict:
+    ''' Ресурс для добавления в базу новых координат врача
+    '''
+    return api_.coords_update_post(request.json)
+
+
+''' =====----- MAIN -----===== '''
+
+if __name__ == '__main__':
+    run(host='0.0.0.0', port=8080, debug=True)
+
+#####=====----- THE END -----=====#########################################
